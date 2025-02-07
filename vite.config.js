0ea4e125f4f907f3acc12f7734ac0d87c067200c
aonoexercist <xercisthegreat@gmail.com>
2025-02-07 16:17:23 +0800

    ziggy config setup

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,7 @@ export default defineConfig({
     resolve: {
         alias: {
             "@": "/resources/js",
+            'ziggy-js': path.resolve('vendor/tightenco/ziggy/dist'),
         },
     },
 });
